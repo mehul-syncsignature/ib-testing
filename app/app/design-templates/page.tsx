883dffccf0bc07e6769fd435c5362be9001df738
mehul-syncsignature <mehul.syncsignature@gmail.com>
2025-09-26 19:27:38 +0530

    fix: build fix

--- app/app/design-templates/page.tsx
+++
@@ -1,8 +0,0 @@
-import BrandSection from "@/modules/Brand/components/BrandSection";
-import React from "react";
-
-const page = () => {
-  return <BrandSection />;
-};
-
-export default page;
--- app/app/my-assets/page.tsx
+++
@@ -1,8 +0,0 @@
-import MyAssetsSection from "@/modules/Brand/components/MyAssets";
-import React from "react";
-
-const page = () => {
-  return <MyAssetsSection />;
-};
-
-export default page;
